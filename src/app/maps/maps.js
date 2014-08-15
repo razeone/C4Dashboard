e12801654b575c2179b86c752041e96bae2aadda
Jorge Raze <razehck@yahoo.com.mx>
2014-08-14 19:44:24 -0500

    Working and taking  object from Remote resource

--- src/app/maps/maps.js
+++ src/app/maps/maps.js
@@ -17,15 +17,39 @@ angular.module( 'ngBoilerplate.maps', [
   });
 })
 
-.controller( 'MapsCtrl', function MapsCtrl( $scope ) {
+.controller( 'MapsCtrl', function MapsCtrl( $scope, $http ) {
+
   // This is simple a demo for UI Boostrap.
+    $scope.request = $http({
+      method: 'GET', 
+      url: 'http://10.105.116.56:4567/getStatesByDivision', 
+      params: {
+        division: 'RT'
+      }
+    });
+    $scope.request.success(function(data) {
+      // this callback will be called asynchronously
+      // when the response is available
+      console.log(data);
+      $scope.polygons = data.json;
+    });
+    $scope.request.error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('Ha habido un error');
+    });
+
+
   $scope.map = {
     center: {
         latitude: 45,
         longitude: -73
     },
     zoom: 8
-};
+  };
+
+
+
 })
 
 ;
