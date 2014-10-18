0a0a5813669ebe88dfdc9baf946e17d63b84f88e
Jorge Raze <razehck@yahoo.com.mx>
2014-10-17 19:14:05 -0500

    Added Intelligence

--- src/app/intelligence/intelligence.js
+++ src/app/intelligence/intelligence.js
@@ -1,6 +1,7 @@
 angular.module( 'ngBoilerplate.intelligence', [
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'google-maps'
 ])
 
 .config(function config( $stateProvider ) {
@@ -16,13 +17,63 @@ angular.module( 'ngBoilerplate.intelligence', [
   });
 })
 
-.controller( 'IntelligenceCtrl', function IntelligenceCtrl( $scope ) {
+.controller('IntelligenceCtrl', function IntelligenceCtrl($scope,$http){
   // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
+  $scope.map = {
+    center: {
+        latitude: 19.493098,
+        longitude: -99.128609
+    },
+    zoom: 12
+  };
+
+ $scope.request = $http({
+    method: 'GET', 
+    url: 'http://172.16.1.128/getDistricts',
+    params: {
+      area: 'LINDAVISTA'
+    }
+  });
+
+  $scope.polygons = [];
+  $scope.request.success(function(data) 
+  {// this callback will be called asynchronously 
+
+      for(var i = 0; i<data.length; i++){
+        tmp = (JSON.parse(data[i].json));
+        var aux = [];  
+        for(var j = 0; j < tmp.coordinates[0][0].length; j++){
+          aux.push({
+            latitude: tmp.coordinates[0][0][j][1],  
+            longitude: tmp.coordinates[0][0][j][0]
+            });
+        }
+
+        $scope.polygons.push({
+          id: i,
+          path: aux,
+          stroke: {
+                    color: '#ff0000',
+                    weight: 0,
+                    opacity: 1
+                },
+                editable: true,
+                geodesic: false,
+                visible: true,
+                fill: {
+                    color: '#ff0000',
+                    opacity: 0.8
+                }
+        });
+        
+    }
+
+    console.log($scope.polygons);
+
+  });
+
+  //console.log(districts);
+
 })
 
 ;
--- src/app/maps/maps.js
+++ src/app/maps/maps.js
@@ -5,7 +5,8 @@ angular.module( 'ngBoilerplate.maps', [
 ])
 
 .config(function config( $stateProvider ) {
-  $stateProvider.state( 'maps', {
+  $stateProvider
+  .state( 'maps', {
     url: '/maps',
     views: {
       "main": {
@@ -20,34 +21,305 @@ angular.module( 'ngBoilerplate.maps', [
 .controller( 'MapsCtrl', function MapsCtrl( $scope, $http ) {
 
   // This is simple a demo for UI Boostrap.
+
+  var N = [];
+  var RT = [];
+  var RR = [];
+  var RE = [];
+  var RU = [];
+  $scope.polygonArray = [];
+
+  var getColor = function(state){
+    switch(state){
+      case 1:
+        return '#ff0000';
+        
+      case 2:
+        return '#00ffff';
+        
+      case 3:
+        return '#ff0000';
+    }
+  };
+
+  function sleep(millis, callback) {
+    setTimeout(function()
+            { callback(); }, millis);
+}
+
+        $scope.request = $http({
+      method: 'GET', 
+      url: 'http://172.16.1.128:8091/quejas/divisiones/'
+    });
+    $scope.request.success(function(data) {
+      // this callback will be called asynchronously
+           for(var k = 0; k<data.length; k++){
+            $scope.polygonArray.push({
+              id: data[k].id,
+              path: null,
+              editable: true,
+              geodesic: false,
+              visible: true,
+              fill: {
+                color: getColor(data[k].status),
+                opacity: 0.8
+              }
+            });
+           }
+          console.log($scope.polygonArray);
+    });
+
+    $scope.request.error(function(data, status, headers, config, error) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('Ha habido un error al recuperar los estados iniciales '+error);
+    });
+
+
     $scope.request = $http({
       method: 'GET', 
-      url: 'http://10.105.116.56:4567/getStatesByDivision', 
+      url: 'http://127.0.0.1:4567/getStatesByDivision',
+      params: {
+        division: 'N'
+      }
+    });
+    $scope.request.success(function(data) 
+{      // this callback will be called asynchronously
+     
+        for(var i = 0; i<data.length; i++){
+          tmp = (JSON.parse(data[i].json));
+          for(var j = 0; j < tmp.coordinates[0][0].length; j++){
+            N.push({
+              latitude: tmp.coordinates[0][0][j][1],  
+              longitude: tmp.coordinates[0][0][j][0]
+              });
+          }
+      }
+
+
+    });
+    $scope.request.error(function(data, status, headers, config, error) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('Ha habido un error al recuperar esta geografía '+error);
+    });
+
+
+    $scope.request = $http({
+      method: 'GET', 
+      url: 'http://127.0.0.1:4567/getStatesByDivision',
       params: {
         division: 'RT'
       }
     });
     $scope.request.success(function(data) {
       // this callback will be called asynchronously
-      // when the response is available
-      console.log(data);
-      $scope.polygons = data.json;
+     
+        for(var i = 0; i<data.length; i++){
+          tmp = (JSON.parse(data[i].json));
+          for(var j = 0; j < tmp.coordinates[0][0].length; j++){
+            RT.push({
+              latitude: tmp.coordinates[0][0][j][1],  
+              longitude: tmp.coordinates[0][0][j][0]
+              });
+          }
+      }
+
     });
-    $scope.request.error(function(data, status, headers, config) {
+    $scope.request.error(function(data, status, headers, config, error) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
-      alert('Ha habido un error');
+      alert('Ha habido un error al recuperar esta geografía '+error);
     });
+
+ $scope.request = $http({
+      method: 'GET', 
+      url: 'http://127.0.0.1:4567/getStatesByDivision',
+      params: {
+        division: 'RE'
+      }
+    });
+    $scope.request.success(function(data) {
+      // this callback will be called asynchronously
+     
+        for(var i = 0; i<data.length; i++){
+          tmp = (JSON.parse(data[i].json));
+          for(var j = 0; j < tmp.coordinates[0][0].length; j++){
+            RE.push({
+              latitude: tmp.coordinates[0][0][j][1],  
+              longitude: tmp.coordinates[0][0][j][0]
+              });
+          }
+      }
+
+    });
+    $scope.request.error(function(data, status, headers, config, error) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('Ha habido un error al recuperar esta geografía '+error);
+    });
+
+
+     $scope.request = $http({
+      method: 'GET', 
+      url: 'http://127.0.0.1:4567/getStatesByDivision',
+      params: {
+        division: 'RR'
+      }
+    });
+    $scope.request.success(function(data) {
+      // this callback will be called asynchronously
+     
+        for(var i = 0; i<data.length; i++){
+          tmp = (JSON.parse(data[i].json));
+          for(var j = 0; j < tmp.coordinates[0][0].length; j++){
+            RR.push({
+              latitude: tmp.coordinates[0][0][j][1],  
+              longitude: tmp.coordinates[0][0][j][0]
+              });
+          }
+      }
+
+    });
+    $scope.request.error(function(data, status, headers, config, error) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('Ha habido un error al recuperar esta geografía '+error);
+    });
+
+         $scope.request = $http({
+      method: 'GET', 
+      url: 'http://127.0.0.1:4567/getStatesByDivision',
+      params: {
+        division: 'RU'
+      }
+    });
+    $scope.request.success(function(data) {
+      // this callback will be called asynchronously
+     
+        for(var i = 0; i<data.length; i++){
+          tmp = (JSON.parse(data[i].json));
+          for(var j = 0; j < tmp.coordinates[0][0].length; j++){
+            RU.push({
+              latitude: tmp.coordinates[0][0][j][1],  
+              longitude: tmp.coordinates[0][0][j][0]
+              });
+          }
+      }
+
+     // var cooHash = {0: RT, 1: RE, 2: RR, 3: RU, 4: N};
+
+      //console.log(cooHash[0]);
+
+      sleep(500, function(){
+        $scope.polygons = [
+            {
+                id: 1,
+                path: RT,
+                stroke: {
+                    color: '#ff0000',
+                    weight: 0,
+                    opacity: 1
+                },
+                editable: true,
+                geodesic: false,
+                visible: true,
+                fill: {
+                    color: '#ff0000',
+                    opacity: 0.8
+                }
+            },
+             {
+                id: 2,
+                path: RE,
+                stroke: {
+                    color: '#ff0000',
+                    weight: 0,
+                    opacity: 1
+                },
+                editable: true,
+                geodesic: false,
+                visible: true,
+                fill: {
+                    color: '#00ff00',
+                    opacity: 0.8
+                }
+              },
+               {
+                id: 3,
+                path: RR,
+                stroke: {
+                    color: '#ff0000',
+                    weight: 0,
+                    opacity: 1
+                },
+                editable: true,
+                geodesic: false,
+                visible: true,
+                fill: {
+                    color: '#ff0000',
+                    opacity: 0.8
+                }
+            },
+             {
+                id: 4,
+                path: RU,
+                stroke: {
+                    color: '#ff0000',
+                    weight: 0,
+                    opacity: 1
+                },
+                editable: true,
+                geodesic: false,
+                visible: true,
+                fill: {
+                    color: '#ff0000',
+                    opacity: 0.8
+                }
+            },
+             {
+                id: 5,
+                path: N,
+                stroke: {
+                    color: '#ff0000',
+                    weight: 0,
+                    opacity: 1
+                },
+                editable: true,
+                geodesic: false,
+                visible: true,
+                fill: {
+                    color: '#ffff00',
+                    opacity: 0.8
+                }
+            }
+        ];
+
+        //console.log($scope.polygons);
+
+      });
+
+
+
+      
+    });
+    $scope.request.error(function(data, status, headers, config, error) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert('Ha habido un error al recuperar esta geografía '+error);
+    });
+
+
+
 
 
   $scope.map = {
     center: {
-        latitude: 45,
-        longitude: -73
+        latitude: 23.54653,
+        longitude: -102.788086
     },
-    zoom: 8
+    zoom: 5
   };
-
 
 
 })
